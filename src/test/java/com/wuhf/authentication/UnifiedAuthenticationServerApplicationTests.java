ce36598625d629ae92ff9455e570270f10b5b998
WUHAIF <420376706@qq.com>
2022-03-21 22:57:52 +0800

    Initial commit

---
+++ src/test/java/com/wuhf/authentication/UnifiedAuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuhf.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnifiedAuthenticationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
